5efa17e0c15688a3acc6d6670acbee7a057327cf
Zachary DeGraw <zde356@sjsd.org>
2019-12-11 09:50:13 -0700

    Add existing file

---
+++ src/components/pages/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import PortfolioContainter from "../portfolio/portfolio-container";
+
+export default function() {
+    return (
+        <div>
+            <PortfolioContainter />
+        </div>
+    );
+}
